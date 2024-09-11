338b4ca6d18124906db468fc283bc9c4f552b0c1
sashary <sashary.morel@asco.org>
2024-09-11 17:45:15 -0400

    Refactor reference resolver & Add footer resolver

--- packages/api/invoke-after-content-publish/context.ts
+++ packages/api/invoke-after-content-publish/context.ts
@@ -1,7 +1,8 @@
 import { ContextPlugin } from "@webiny/api/plugins/ContextPlugin";
 import { EventBridgeClient, PutEventsCommand } from "@webiny/aws-sdk/client-eventbridge";
-import { CmsContext, CmsEntry } from "@webiny/api-headless-cms/types";
+import { CmsContext } from "@webiny/api-headless-cms/types";
 import { ascoEnvs, awsconfig } from 'awsconfig';
+import { resolveReference, flattenEntry } from '../helper/referenceResolver';
 
 export const createContext = () => {
     return new ContextPlugin<CmsContext>(async context => {
@@ -17,27 +18,8 @@ export const createContext = () => {
                     return;
                 }
 
-                // Resolve special cases with references.
-                // Person will always contain a personsPage
-                if (modelId.toLowerCase() == "person") {
-                    const manager = await context.cms.getEntryManager("personsPage");
-                    const [item] = await manager.getPublishedByIds([entry.values["personsPage"]["entryId"]]);
-                    entry.values["personsPage"] = flattenEntry(item)
-                }
-
-                if (modelId.toLowerCase() == "personspage") {
-                    const personsIds = entry.values["persons"]
-                    const manager = await context.cms.getEntryManager("person");
-                    const personsValues = await manager.getPublishedByIds(personsIds.map((item: { [x: string]: any; }) => item["entryId"]));
-                    const persons = []
-                    for (const item of personsValues) {
-                        persons.push(flattenEntry(item));
-                    }
-                    entry.values["persons"] = persons
-                }
-
                 const eventBusArn = awsconfig[env].eventbus;
-                const flattenedData = flattenEntry(entry); // Custom function to flatten the entry
+                const flattenedData = flattenEntry(entry);
                 console.log("Model from context", model);
                 console.log("Entry from context", entry);
 
@@ -46,6 +28,10 @@ export const createContext = () => {
                     console.log("Environment can only be 'dev', 'stage', or 'prod'; skipping EventBridge call.");
                     return;
                 }
+                
+                // Resolve special cases with references.
+                resolveReference(context, entry, modelId);
+
                 // Construct event
                 const event = {
                     Entries: [
@@ -73,19 +59,4 @@ export const createContext = () => {
             }
         });
     });
-};
-
-/**
- * Flatten the entry object
- * Assumes `entry` is an object with `values` containing nested fields.
- */
-const flattenEntry = (entry: CmsEntry) => {
-    // Extract values and other fields from entry
-    const { values, ...otherFields } = entry;
-
-    // Flatten `values` and merge with other fields
-    return {
-        ...values,
-        ...otherFields
-    };
 };
