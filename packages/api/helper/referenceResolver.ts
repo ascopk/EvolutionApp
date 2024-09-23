f8a4a644e7a6bb54ead756e44973569f7fec58b4
Sashary Morel <28787380+sashary@users.noreply.github.com>
2024-09-23 08:45:29 -0400

    Refactor reference resolver & Add footer resolver (#86)

---
+++ packages/api/helper/referenceResolver.ts
@@ -0,0 +1,63 @@
+import { CmsContext, CmsEntry, CmsEntryValues } from "@webiny/api-headless-cms/types";
+
+enum ModelName {
+    Person = 'person',
+    Person_Page = "personspage",
+    News = "newsRelease",
+    None = "none"
+}
+
+/*
+ Webiny does not automatically resolve references during lifecycles, meaning we must identify a reference
+ and resolve them during the trigger.
+*/
+export const resolveReference = async (context: CmsContext, entry: CmsEntry<CmsEntryValues>, modelId: string) => {
+    const modelIdLowerCase = modelId.toLowerCase();
+    const Model: ModelName = modelIdLowerCase as ModelName || ModelName.None
+
+    if (Model === ModelName.Person) {
+        const manager = await context.cms.getEntryManager("personsPage");
+        const [item] = await manager.getPublishedByIds([entry.values["personsPage"]["entryId"]]);
+        if (item) {
+            entry.values["personsPage"] = flattenEntry(item)
+        } else {
+            entry.values["personsPage"] = null
+        }
+    }
+
+    if (Model === ModelName.Person_Page) {
+        const personsIds = entry.values["persons"]
+        const manager = await context.cms.getEntryManager("person");
+        const personsValues = await manager.getPublishedByIds(personsIds.map((item: { [x: string]: any; }) => item["entryId"]));
+        const persons = []
+        for (const item of personsValues) {
+            if (item) {
+                persons.push(flattenEntry(item));
+            }
+        }
+        entry.values["persons"] = persons
+    }
+
+    if (Model === ModelName.News) {
+        const manager = await context.cms.getEntryManager("aboutAsco");
+        const [item] = await manager.getPublishedByIds([entry.values["aboutAscoFooter"]["entryId"]]);
+        if (item) {
+            entry.values["aboutAscoFooter"] = flattenEntry(item)
+        } else {
+            entry.values["aboutAscoFooter"] = null
+        }
+    }
+    
+    return entry;
+}
+
+// Extracts values from entry and flatens it alongside other fields
+export const flattenEntry = (entry: CmsEntry) => {
+    const { values, ...otherFields } = entry;
+
+    return {
+        ...values,
+        ...otherFields
+    };
+};
+
--- packages/api/invoke-after-content-delete/context.ts
+++ packages/api/invoke-after-content-delete/context.ts
@@ -1,7 +1,8 @@
 import { ContextPlugin } from "@webiny/api/plugins/ContextPlugin";
 import { EventBridgeClient, PutEventsCommand } from "@webiny/aws-sdk/client-eventbridge";
-import { CmsContext, CmsEntry } from "@webiny/api-headless-cms/types";
+import { CmsContext } from "@webiny/api-headless-cms/types";
 import { ascoEnvs, awsconfig } from 'awsconfig';
+import { flattenEntry } from '../helper/referenceResolver';
 
 export const createContext = () => {
     return new ContextPlugin<CmsContext>(async context => {
@@ -15,13 +16,15 @@ export const createContext = () => {
                     console.log("Ignoring draft mode entries");
                     return;
                 }
+
                 // Only proceed if environment is 'dev', 'stage', or 'prod'
                 if (!ascoEnvs.includes(env)) {
                     console.log("Environment can only be 'dev', 'stage', or 'prod'; skipping EventBridge call.");
                     return;
                 }
+
                 const eventBusArn = awsconfig[env].eventbus;
-                const flattenedData = flattenEntry(entry); // Custom function to flatten the entry
+                const flattenedData = flattenEntry(entry);
 
                 // Construct event
                 const event = {
@@ -49,19 +52,4 @@ export const createContext = () => {
             }
         });
     });
-};
-
-/**
- * Flatten the entry object
- * Assumes `entry` is an object with `values` containing nested fields.
- */
-const flattenEntry = (entry: CmsEntry) => {
-    // Extract values and other fields from entry
-    const { values, ...otherFields } = entry;
-
-    // Flatten `values` and merge with other fields
-    return {
-        ...values,
-        ...otherFields
-    };
 };
